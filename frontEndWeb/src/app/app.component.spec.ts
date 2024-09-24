7b9d2dd1ee5e9462ea11b22e0fd3793fe9e313f9
Jorge Torrado <jorgeetorradoforero@gmail.com>
2024-09-24 17:43:36 -0500

    menu login y registro

--- frontEndWeb/src/app/app.component.spec.ts
+++ frontEndWeb/src/app/app.component.spec.ts
@@ -1,27 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { logInComponent } from './logIn.component';
 
-describe('AppComponent', () => {
+describe('logInComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [logInComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(logInComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'frontEndWeb' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(logInComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontEndWeb');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(logInComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontEndWeb');
