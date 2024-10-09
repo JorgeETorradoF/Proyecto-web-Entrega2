760a663cb10cf9bcdbdf9a8422d4b03ebfaa6e95
FTTE27 <vasquezv.juanandres@gmail.com>
2024-10-09 14:38:04 -0500

    Funciona el despliegue

--- frontEndWeb/src/app/app-routing.module.ts
+++ frontEndWeb/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContratosArrendadorComponent } from './contratos-arrendador/contratos-arrendador.component';
 import { LoginComponent } from './login/login.component'; // Componente de login
 import { ContratosArrendatarioComponent } from './contratos-arrendatario/contratos-arrendatario.component';
+import { PrincipalArrendadorComponent } from './principal-arrendador/principal-arrendador.component';
+import { PrincipalArrendatarioComponent } from './principal-arrendatario/principal-arrendatario.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'arrendador/:idArrendador/contratos', component: ContratosArrendadorComponent },
-  {path: 'arrendatario/:idArrendatario/contratos', component: ContratosArrendatarioComponent}
+  { path: 'arrendatario/:idArrendatario/contratos', component: ContratosArrendatarioComponent},
+  { path: 'arrendador/:idArrendador', component: PrincipalArrendadorComponent },
+  { path: 'arrendatario/:idArrendatario', component: PrincipalArrendatarioComponent },
   // Otras rutas...
 ];
 
---
+++ frontEndWeb/src/app/principal-arrendador/principal-arrendador.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-principal-arrendador',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal-arrendador.component.html',
+  styleUrl: './principal-arrendador.component.css'
+})
+export class PrincipalArrendadorComponent implements OnInit {
+  idArrendador!: string;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.idArrendador = this.route.snapshot.paramMap.get('idArrendador')!;
+  }
+
+  navigateToCalificar() {
+    this.router.navigate([`/arrendador/${this.idArrendador}/calificar`]);
+  }
+
+  navigateToVerContratos() {
+    this.router.navigate([`/arrendador/${this.idArrendador}/contratos`]);
+  }
+
+  navigateToVerPropiedades() {
+    this.router.navigate([`/arrendador/${this.idArrendador}/ver-propiedades`]);
+  }
+}
